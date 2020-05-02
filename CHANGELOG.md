f94d86d2ecec775cf6314994730cd813934d1930
DarSpace <45310234+DarSpace@users.noreply.github.com>
2020-05-02 10:46:04 +0200

    Main

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,27 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.2.0] - 2020-05-02
+
+### Changes
+
+    -
+
+### Added
+
+    - adding text and graphics in the file Main
+
+## [1.1.0] - 2020-04-16
+
+### Changes
+
+    - new logo splashscreen
+    - backgrand splashscreen
+
+### Added
+
+    - splashscreen
+
 ## [1.0.1] - 2020-04-15
 
 ### Changes
--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,20 +1,48 @@
 import React, { FC } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import styled from 'styled-components/native';
 
 import ButtomTabs from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
 
 interface IMainProps {}
 
-// <Image source={require("profile.png")} />;
-
 const Main: FC<IMainProps> = (props) => {
   return (
-    <View>
-      <Text>Main Screen</Text>
-    </View>
+    <ScrollView>
+      <StatusBar />
+      <View style={{ padding: 15 }}>
+        <Image style={{ width: 100, height: 100, borderRadius: 50 }} source={require('../../assets/splashlogo.png')} />
+        <Text style={{ fontSize: 18, textAlign: 'justify' }}>
+          The Kaiser roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread
+          roll, originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating
+          from the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The
+          Kaiser roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The Kaiser
+          roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The Kaiser
+          roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The Kaiser
+          roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The Kaiser
+          roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The Kaiser
+          roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture. The Kaiser
+          roll (Emperor roll, German: Kaisersemmel), also called a Vienna roll (Wiener Kaisersemmel; as made by hand also: Handsemmel, Slovene: kajzerica) or a hard roll, is a typically crusty round bread roll,
+          originally from Austria. It is made from white flour, yeast, malt, water and salt, with the top side usually divided in a symmetric pattern of five segments, separated by curved superficial cuts radiating from
+          the centre outwards or folded in a series of overlapping lobes resembling a crown. The crisp Kaisersemmel is a traditional Austrian food officially approved by the Federal Ministry of Agriculture.
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 
